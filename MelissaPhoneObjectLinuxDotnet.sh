88cde5b835392c2bf4267b1f0ca0030d7534163a
Melissa <>
2025-06-10 11:54:54 -0700

    Tested successfully using Melissa data files for 2025.06.

--- MelissaPhoneObjectLinuxDotnet.sh
+++ MelissaPhoneObjectLinuxDotnet.sh
@@ -52,7 +52,7 @@ while [ $# -gt 0 ] ; do
 done
 
 # ######################### Config ###########################
-RELEASE_VERSION='2025.05'
+RELEASE_VERSION='2025.06'
 ProductName="DQ_PHONE_DATA"
 
 # Uses the location of the .sh file 
@@ -242,7 +242,7 @@ printf "\nAll file(s) have been downloaded/updated!\n"
 # Build project
 printf "\n=========================== BUILD PROJECT ==========================\n"
 
-dotnet publish -f="net7.0" -c Release -o $BuildPath MelissaPhoneObjectLinuxDotnet/MelissaPhoneObjectLinuxDotnet.csproj
+dotnet publish -f="net8.0" -c Release -o $BuildPath MelissaPhoneObjectLinuxDotnet/MelissaPhoneObjectLinuxDotnet.csproj
 
 # Run project
 if [ -z "$phone" ];
